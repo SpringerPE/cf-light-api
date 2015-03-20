13c486158d2fbef3852c22acc37527b978fbcd2f
Ryan Conway <ryan@rjc.cc>
2015-03-20 10:55:53 +0000

    Implement filtering by organisation name for /v1/apps

--- lib/sinatra/cf_light_api.rb
+++ lib/sinatra/cf_light_api.rb
@@ -1,10 +1,19 @@
+require 'json'
+
 module Sinatra
   module CfLightAPI
 
     def self.registered(app)
-      app.get '/v1/apps/?' do
+      app.get '/v1/apps/?:org?' do
+        all_apps = JSON.parse(REDIS.get("#{ENV['REDIS_KEY_PREFIX']}:apps"))
+
         content_type :json
-        REDIS.get "#{ENV['REDIS_KEY_PREFIX']}:apps"
+
+        if params[:org]
+          return all_apps.select{|an_app| an_app['org'] == params[:org]}.to_json
+        end
+
+        return all_apps.to_json
       end
 
       app.get '/v1/orgs/?' do
