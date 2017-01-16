9036895f6fe455c1eefbd3c80d80b8e76f83b3bd
Ryan Conway <ryan@rjc.cc>
2017-01-16 15:42:44 +0000

    Send org quota data to Graphite.

--- lib/cf_light_api/worker.rb
+++ lib/cf_light_api/worker.rb
@@ -108,6 +108,15 @@ class CFLightAPIWorker
     end
   end
 
+  def send_org_quota_data_to_graphite(org_name, quota)
+    graphite_base_key = "cf_orgs.#{ENV['CF_ENV_NAME']}.#{org_name}"
+    @logger.info "  Exporting org quota statistics to Graphite, path '#{graphite_base_key}'"
+
+    quota.keys.each do |key|
+      @graphite.metrics "#{graphite_base_key}.quota.#{key}" => quota[key]
+    end
+  end
+
   def put_in_redis(key, data)
     REDIS.set key, data.to_json
   end
@@ -155,15 +164,18 @@ class CFLightAPIWorker
           formatted_orgs = @orgs.map do |org|
             quota = @quotas.find{|a_quota| a_quota['metadata']['guid'] == org['entity']['quota_definition_guid']}
 
+            quota = {
+              :total_services => quota['entity']['total_services'],
+              :total_routes   => quota['entity']['total_routes'],
+              :memory_limit   => quota['entity']['memory_limit'] * 1024 * 1024
+            }
+
+            send_org_quota_data_to_graphite(org['entity']['name'], quota) if @graphite
+
             {
               :guid => org['metadata']['guid'],
               :name  => org['entity']['name'],
-              :quota => {
-                :name           => quota['entity']['name'],
-                :total_services => quota['entity']['total_services'],
-                :total_routes   => quota['entity']['total_routes'],
-                :memory_limit   => quota['entity']['memory_limit'] * 1024 * 1024
-              }
+              :quota => quota
             }
           end
 
